6987a827403b3bdcf653e1911d85f4761ca4b587
rafaelabou1999 <rafaelabourdette@gmail.com>
2025-02-05 18:11:21 -0300

    :tada: first commmit

---
+++ public/index.php
@@ -0,0 +1,17 @@
+<?php include __DIR__ . '/../src/inc/Header.php' ?>
+    <main >
+        <div class="bg-secondary bg-opacity-25" style="height:350px; width:100%; padding-left: 20%;padding-top:3.5%;" >
+            <h1>Welcome to online TechStore books</h1>
+            <p class="pt-5 fs-2 text-secondary ">This site has been made using PHP with MYSQL!</p>
+            <p class="pt-1 fs-2 text-secondary ">The layout use Bootstrap to make it more responsive. It's just a simple web!</p>
+        </div>
+        <div>
+            <h3 class="text-center fw-bold text-secondary" style="padding-top:5%;">Latest Books</h3>
+        </div>
+    </main>
+
+
+<?php include __DIR__ . '/../src/inc/Footer.php' ?>
+
+
+
---
+++ src/inc/Header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap 5 CSS -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>Welcome</title>
+</head>
+<body>
+    <header class="p-4 bg-dark bg-gradient text-white d-flex flex-row justify-content-around align-items-center">
+        <h4 class="">TechStore</h4>
+        <div class="d-flex flex-row justify-content-around">
+            <a class="text-white px-4 tracking-wide" href="">Publisher</a>
+            <a class="text-white px-4 tracking-wide" href="">Books</a>
+            <a class="text-white px-4 tracking-wide" href="">Contact</a>
+            <a class="text-white px-4 tracking-wide" href="">My Cart</a>
+        </div>
+    </header>
