0ad21227650c27091704b98a1b3348d146a5eb69
rafaelabou1999 <rafaelabourdette@gmail.com>
2025-02-06 15:35:33 -0300

    :construction: Adding dynamics to books section

--- src/Views/bookstore.php
+++ src/Views/bookstore.php
@@ -5,15 +5,22 @@ include __DIR__ . '/../inc/Header.php';
 $pdo = new PDO("mysql:host=localhost;dbname=books", "root", "1234");
 $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 
-$stmt = $pdo->prepare("SELECT image FROM bookstore");
+$stmt = $pdo->prepare("SELECT * FROM bookstore");
 $stmt->execute();
-$images = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
 
 <div class="d-flex flex-row justify-content-center">
-    <?php foreach($images AS $image):?>
-        <img class="me-5 border border-secondary" src="<?= $image['image'] ?>" width="230px" height="280px" style="object-fit:cover;margin-top:5%;"/>
+    <?php foreach($results AS $result):?>
+        <?php  
+        $query_params = http_build_query([
+            "id" => $result['id'],
+            "title" => $result['title']
+         ]);
+         $url = "http://localhost/bookstore/src/Views/details.php?" . $query_params;
+         ?>
+        <a href="<?= $url ?>"><img class="me-5 border border-secondary" src="<?= $result['image'] ?>" width="230px" height="280px" style="object-fit:cover;margin-top:45%;"/></a>
     <?php endforeach; ?>
 </div>
 
