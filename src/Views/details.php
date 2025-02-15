596b50a4d6ea430ed7a83017bea5cb09d4176259
rafaelabou1999 <rafaelabourdette@gmail.com>
2025-02-15 20:57:59 -0300

    Organizing code

--- src/Views/details.php
+++ src/Views/details.php
@@ -1,7 +1,6 @@
 <?php 
 $is_home = false;
 require_once dirname(__DIR__,2) . '/config.php'; 
-require_once __DIR__ . '/../inc/books.php'; 
 include __DIR__ . '/../inc/Header.php'; 
 
 $pdo = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASSWORD, [
